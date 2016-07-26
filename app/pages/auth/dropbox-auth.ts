6a0169f573ce0957bd97c61b34c9d898b86f4ad5
Sean Ray <seankennethray@gmail.com>
2016-07-25 22:44:47 -0500

    Attempting to add dropbox authentication
    Apparently InAppBrowser won't launch when running with `ionic serve`
    so I might need to either start testing in an emulator or find another
    workaround.

---
+++ app/pages/auth/dropbox-auth.ts
@@ -0,0 +1,57 @@
+import {Component} from '@angular/core';
+import {Platform, NavParams, ViewController} from 'ionic-angular';
+import {Alert, NavController} from 'ionic-angular';
+import * as appKey from './app-key';
+import {InAppBrowser} from 'ionic-native';
+
+@Component({
+  templateUrl: './build/pages/auth/dropbox-auth.html'
+})
+export class DropboxAuthModal {
+
+  private authUrl:string;
+  private redirectURI:string;
+  private accessToken:string;
+
+  constructor(
+      public platform: Platform,
+      public params: NavParams,
+      public viewCtrl: ViewController,
+      public nav: NavController
+  ) {
+    this.redirectURI = 'http://localhost';
+    this.authUrl = 'https://www.dropbox.com/oauth2/authorize?client_id=' + appKey + '&redirect_uri=' + this.redirectURI + '&response_type=token';
+  }
+
+  login(){
+    return new Promise((resolve, reject) => {
+
+      let browser = InAppBrowser.open(this.authUrl, '_blank');
+
+      browser.addEventListener('loadstart', (event) => {
+
+        //Ignore the dropbox authorize screen
+        if(event.url.indexOf('oauth2/authorize') > -1){
+          return;
+        }
+
+        //Check the redirect uri
+        if(event.url.indexOf(this.redirectURI) > -1 ){
+          browser.removeEventListener('exit', (event) => {});
+          browser.close();
+          let token = event.url.split('=')[1].split('&')[0];
+          this.accessToken = token;
+          resolve(event.url);
+        } else {
+          reject("Could not authenticate");
+        }
+
+      });
+
+    });
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
+}
--- app/pages/books/backup-restore.ts
+++ app/pages/books/backup-restore.ts
@@ -1,14 +1,21 @@
 import {Component} from '@angular/core'
 import {Modal, NavController} from 'ionic-angular'
-import {EditBookModal} from './edit-book'
-import {Book} from './Book';
-import * as storager from '../../storager/storager';
+import {DropboxAuthModal} from '../auth/dropbox-auth'
 
 @Component({
   templateUrl: './build/pages/books/backup-restore.html'
 })
 export class BackupRestorePage {
 
+  private authorized = false;
+
   constructor(public nav: NavController) {
+    if(!this.authorized) {
+      let modal = Modal.create(DropboxAuthModal);
+      modal.onDismiss(data => {
+        console.log(data);
+      });
+      this.nav.present(modal);
+    }
   }
 }
